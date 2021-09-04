ba0c05a292a0c73ac7db27b8589e53bdd07ead48
NuymakStone <79128084+NuymakStone@users.noreply.github.com>
2021-09-04 16:52:03 +0800

    Update Strings Scanner.c

--- Strings Scanner.c
+++ Strings Scanner.c
@@ -30,6 +30,8 @@ char* x= "kc((k";
 char* y= "JNativeHook";
 char* k= "144.217.241.181";
 char* j= "/tcpnodelaymod/COM1";
+char* z= "[la/";
+char* A= "Z9ByByByByB";
 
 char a1[255]; 
 char b1[255];
@@ -56,183 +58,201 @@ char x1[255];
 char y1[255];
 char k1[255];
 char j1[255];
-printf("Strings Scanner by DonTopoGigio_ (ONLY 1.8)\nWait a few minutes...\n");
+char z1[255];
+char A1[255];
+
+printf("Strings Scanner by NuymakStone (ONLY 1.8)\n请等待一分钟...\n");
+printf("它是如何工作的：\n下载进程黑客 ( https://processhacker.sourceforge.io/ )\n安装并打开\n右键单击 Javaw.exe ---> 属性 ---> 内存 ---> 去掉“隐藏空闲区域”上的“√”\n在相同的页面中，转到“字符串...”，在“最小值”附近写下 4，然后在“图像”和“映射”上打勾并确认“确定”\n转到“保存...”并将文件保存在与此文件相同的目录中（“String Scanner.exe”）\n打开“String Scanner.exe”，它将为您提供找到的客户端名称的输出。\n");
+printf("By NuymakStone\n"); 
 //AVIX a
 FILE* Filea;
 if((Filea = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(a1,254,Filea)) 
 printf("%s\n", strstr(a1, a)); 
 //TrumpClient b
 FILE* Fileb;
 if((Fileb = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(b1,254,Fileb))
 printf("%s\n", strstr(b1, b));
 //dg82fo.pw c
 FILE* Filec;
 if((Filec = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(c1,254,Filec))
 printf("%s\n", strstr(c1, c));
 //nG@W d
 FILE* Filed;
 if((Filed = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(d1,254,Filed))
 printf("%s\n", strstr(d1, d));
 //G0ttaDipMen.java e
 FILE* Filee;
 if((Filee = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(e1,254,Filee))
 printf("%s\n", strstr(e1, e));
 //Harambe.png f
 FILE* Filef;
 if((Filef = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(f1,254,Filef))
 printf("%s\n", strstr(f1, f));
 //czaarek99 g
 FILE* Fileg;
 if((Fileg = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(g1,254,Fileg))
 printf("%s\n", strstr(g1, g));
 //+(M0G.V h
 FILE* Fileh;
 if((Fileh = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(h1,254,Fileh))
 printf("%s\n", strstr(h1, h));
 //dinkio i
 FILE* Filei;
 if((Filei = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(i1,254,Filei))
 printf("%s\n", strstr(i1, i));
 //Sa_Vc l
 FILE* Filel;
 if((Filel = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(l1,254,Filel))
 printf("%s\n", strstr(l1, l));
 //5d@56 m
 FILE* Filem;
 if((Filem = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(m1,254,Filem))
 printf("%s\n", strstr(m1, m));
 //TCNH$1 n
 FILE* Filen;
 if((Filen = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(n1,254,Filen))
 printf("%s\n", strstr(n1, n));
 //onetap.cc o
 FILE* Fileo;
 if((Fileo = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(o1,254,Fileo))
 printf("%s\n", strstr(o1, o));
 //bspkrs.IlIIIlIlIllIIlllIllIllIII p
 FILE* Filep;
 if((Filep = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(p1,254,Filep))
 printf("%s\n", strstr(p1, p));
 //com/sun/jna/z/Main q
 FILE* Fileq;
 if((Fileq = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(q1,254,Fileq))
 printf("%s\n", strstr(q1, q));
 ///a.class:::0 r
 FILE* Filer;
 if((Filer = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(r1,254,Filer))
 printf("%s\n", strstr(r1, r));
 //hi.a2 s
 FILE* Files;
 if((Files = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(s1,254,Files))
 printf("%s\n", strstr(s1, s));
 //0SO1Lk2KASxzsd t
 FILE* Filet;
 if((Filet = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(t1,254,Filet))
 printf("%s\n", strstr(t1, t));
 //yCcADi u
 FILE* Fileu;
 if((Fileu = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(u1,254,Fileu))
 printf("%s\n", strstr(u1, u));
 //74.91.125.194 v 
 FILE* Filev;
 if((Filev = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(v1,254,Filev))
 printf("%s\n", strstr(v1, v));
 //kc(g9 w
 FILE* Filew;
 if((Filew = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(w1,254,Filew))
 printf("%s\n", strstr(w1, w));
 //kc((k x
 FILE* Filex;
 if((Filex = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(x1,254,Filex))
 printf("%s\n", strstr(x1, x));
 //JNativeHook y
 FILE* Filey;
 if((Filey = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(y1,254,Filey))
 printf("%s\n", strstr(y1, y));
 //144.217.241.181 k
 FILE* Filek;
 if((Filek = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(k1,254,Filek)) 
 printf("%s\n", strstr(k1, k)); 
+//VAPE
+FILE* Filez;
+if((Filez = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
+}
+while(fgets(z1,254,Filez)) 
+printf("%s\n", strstr(z1, z));  
+//VapeLite
+FILE* FileA;
+if((FileA = fopen("Search results.txt", "r"))==NULL)
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
+}
+while(fgets(A1,254,FileA)) 
+printf("%s\n如果出现了此行文字，请你确认是否为1.8.9forge客户端或者香草客户端。\n", strstr(A1, A));  
 //tcpnodelaymod/COM1 j
 FILE* Filej;
 if((Filej = fopen("Search results.txt", "r"))==NULL)
-{	fprintf(stderr, "errore nell'apertura del file\n");
+{	fprintf(stderr, "没有找到Search results.txt，请把exe放在文件的同一目录。\n");
 }
 while(fgets(j1,254,Filej)) 
 printf("%s\n", strstr(j1, j)); 
 system("PAUSE");
 }
-
 
